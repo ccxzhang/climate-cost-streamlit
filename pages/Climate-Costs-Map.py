727258115ec58d4afc75e1a68df37d44236392bf
Charlie Zhang <cz317@georgetown.edu>
2023-10-12 17:35:53 -0400

    update map features.

--- pages/Climate-Costs-Map.py
+++ pages/Climate-Costs-Map.py
@@ -7,23 +7,29 @@ from streamlit_folium import st_folium, folium_static
 import geopandas as gpd
 from datetime import datetime, timedelta
 from dateutil.relativedelta import relativedelta
-import random
 
+st.set_page_config(layout="wide")
 # Specify the filepath and read
 cc_file = os.getcwd() + "/data/cc_cleaned.csv"
 shp_file = os.getcwd() + "/data/gadm_pacific/"
 info_file = os.getcwd() + "/data/country_info.csv"
 
-df = pd.read_csv(cc_file).drop("Unnamed: 0", axis=1)
+@st.cache_data
+def load_data(path) -> pd.DataFrame:
+    df = pd.read_csv(path).drop("Unnamed: 0", axis=1)
+    return df
+
+
+df = load_data(cc_file) 
+info = load_data(info_file)
 shp = gpd.read_file(shp_file)
-info = pd.read_csv(info_file).drop("Unnamed: 0", axis=1)
 
 scenario_list = df["scenario"].unique().tolist()
 adaptation_list = df["case"].unique().tolist()
 
 # Header and
 st.header("Climate Costs Map")
-left_panel, right_panel = st.columns([2, 8])
+left_panel, right_panel = st.columns([0.25, 0.75])
 
 with left_panel:
     select_time = st.select_slider(
@@ -42,8 +48,11 @@ with left_panel:
 
     sea_level_option = st.selectbox(
         "Sea Level Rise Scenario",
-        scenario_list
+        scenario_list,
+        label_visibility="visible"
     )
+    on = st.toggle('Display Raw Data')
+
 # Apply Filter
 df = df[(df.year == select_time) & (df.case == adaptation_option) & (
     df.ssp == ssp_option) & (df.scenario == sea_level_option)].reset_index(drop=True)
@@ -51,21 +60,35 @@ df = df.merge(shp[["GID_1", "geometry"]], how="left",
               left_on="adm1", right_on="GID_1")
 df = df.merge(info, how="left", on="adm1")
 gdf = gpd.GeoDataFrame(df)
+select_cols_df = [col for col in df.columns if col not in
+                  ["geometry", "GID_1"]
+]
+
+max_total_cost = df["total_cost"].max()
+
 
 m = folium.Map(location=(4, 160), zoom_start=4)
 for _, r in gdf.iterrows():
 
     gj = gpd.GeoSeries(r["geometry"]).simplify(tolerance=0.001).to_json()
-    folium.GeoJson(data=gj).add_to(m)
+    fillOpacity = r["total_cost"]/max_total_cost
+    folium.GeoJson(data=gj,
+                   style_function=lambda x, fillOpacity=fillOpacity: {
+                       "fillColor": "red",
+                       "fillOpacity": fillOpacity,
+                       "weight": 1}).add_to(m)
     folium.CircleMarker(
         location=[r["geometry"].centroid.y, r["geometry"].centroid.x],
         radius=1,
         color="blue",
         popup=folium.Popup((
             "Total Cost: ${cost}<br>"
-            "GDP in 2019: {gdp}<br>").format(cost=str(r["total_cost"]), gdp=str(r["K_2019"])),
+            "GDP in 2019: {gdp}<br>").format(cost=str(r["total_cost_num"]), gdp=str(r["K_2019"])),
             min_width=200,
             max_width=200)).add_to(m)
 
 with right_panel:
-    folium_static(m, width=900)
+    folium_static(m, width=1085)
+    if on:
+        st.write(df[select_cols_df], width=1000)
+    
